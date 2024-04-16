eba15ff1e4582cc1dc87d71fca4aaf45b1340e1a
fzaninotto <fzaninotto@gmail.com>
2024-04-16 10:25:02 +0200

    Use external library for search term highlighting

--- src/compositions/HighlighhtSearchTerm.tsx
+++ src/compositions/HighlighhtSearchTerm.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useListContext } from "react-admin";
 import { useLocation } from "react-router-dom";
 import debounce from "lodash/debounce";
+import { highlightSearchTerm } from "highlight-search-term";
 
 /**
  * Watch the current search term and highlight it in the list of compositions
@@ -16,42 +17,13 @@ export const HighlightSearchTerm = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (!CSS.highlights) return; // disable feature on Firefox as it does not support CSS Custom Highlight API
-    CSS.highlights.clear();
-    if (!search) return;
-    highlightSearchTerm(
+    debounceHighlightSearchTerm({
       search,
-      ".MuiListItemText-primary>div, .MuiListItemText-secondary, [contenteditable=true]"
-    );
+      selector: ".MuiList-root, [contenteditable=true]",
+    });
   }, [location.pathname, search]);
   return null;
 };
 
 // debounce allows to delay the highlight, which allows the composition to render before the highlight
-const highlightSearchTerm = debounce((search: string, selector: string) => {
-  const ranges: Range[] = [];
-  const elements = document.querySelectorAll(selector);
-  Array.from(elements).forEach((element) => {
-    ranges.push(...getRangesForSearchTermInElement(element as Element, search));
-  });
-  if (ranges.length === 0) return;
-  const highlight = new Highlight(...ranges); // eslint-disable-line no-undef
-  // create a CSS highlight that can be styled with the ::highlight(search) pseudo-element
-  CSS.highlights.set("search", highlight);
-}, 100);
-
-const getRangesForSearchTermInElement = (element: Element, search: string) => {
-  const ranges: Range[] = [];
-  if (!element.firstChild) return ranges;
-  const text = element.textContent?.toLowerCase() || "";
-  let start = 0;
-  let index;
-  while ((index = text.indexOf(search, start)) >= 0) {
-    const range = new Range();
-    range.setStart(element.firstChild, index);
-    range.setEnd(element.firstChild, index + search.length);
-    ranges.push(range);
-    start = index + search.length;
-  }
-  return ranges;
-};
+const debounceHighlightSearchTerm = debounce(highlightSearchTerm, 100);
